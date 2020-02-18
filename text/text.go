ef2c7df39adaf9478de7a45b1032a89e24b8da31
Maas Lalani <maaslalani1@gmail.com>
2020-02-18 09:07:30 -0500

    go fmt

--- text/text.go
+++ text/text.go
@@ -14,11 +14,11 @@ import (
 )
 
 const (
-	MAX_HEIGHT      = 75
-	MAX_WIDTH       = 0
-	RESIZE_STRATEGY = resize.Lanczos2
-	ASCII           = " .,:~=+?I7$ZO8DNM"
-	MODIFIER        = 2.2
+	maxHeight      = 75
+	maxWidth       = 0
+	resizeStrategy = resize.Lanczos2
+	asciiMap       = " .,:~=+?I7$ZO8DNM"
+	modifier       = 2.2
 )
 
 func Command(filepath string) {
@@ -35,28 +35,28 @@ func Command(filepath string) {
 		panic("Unable to decode file")
 	}
 
-	img = resize.Resize(MAX_WIDTH, MAX_HEIGHT, img, RESIZE_STRATEGY)
+	img = resize.Resize(maxWidth, maxHeight, img, resizeStrategy)
 
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
 
 	for y := 0; y < height; y += 2 {
-		for x := 0; x < width; x += 1 {
+		for x := 0; x < width; x++ {
 			pixel := img.At(x, y)
 			red, green, blue, _ := pixel.RGBA()
 
-			grayRed := 0.2125 * math.Pow(float64(red), MODIFIER)
-			grayBlue := 0.7154 * math.Pow(float64(blue), MODIFIER)
-			grayGreen := 0.0721 * math.Pow(float64(green), MODIFIER)
+			grayRed := 0.2125 * math.Pow(float64(red), modifier)
+			grayBlue := 0.7154 * math.Pow(float64(blue), modifier)
+			grayGreen := 0.0721 * math.Pow(float64(green), modifier)
 
-			grayPixel := math.Pow(grayRed+grayBlue+grayGreen, 1.0/MODIFIER)
+			grayPixel := math.Pow(grayRed+grayBlue+grayGreen, 1.0/modifier)
 			grayColor := color.Gray{uint8(uint16(grayPixel+0.5) >> 8)}
 
 			normalized := float32(grayColor.Y) / 255.0
-			normalized *= float32(len(ASCII) - 1.0)
+			normalized *= float32(len(asciiMap) - 1.0)
 
 			index := int(normalized)
-			fmt.Print(ASCII[index : index+1])
+			fmt.Print(asciiMap[index : index+1])
 		}
 		fmt.Println()
 	}
